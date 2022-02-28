b78d0205e635c196774c201d38d4cb431eb70071
Ross <roswellpataro@gmail.com>
2022-02-27 20:28:44 -0700

    Updated Player movement

--- gimmStudio/Assets/Scripts/PlayerManager.cs
+++ gimmStudio/Assets/Scripts/PlayerManager.cs
@@ -7,6 +7,8 @@ using Photon.Pun;
 using System;
 using System.Collections;
 
+using UnityEngine.InputSystem;
+
 namespace Com.MyCompany.MyGame
 {
     /// <summary>
@@ -15,6 +17,9 @@ namespace Com.MyCompany.MyGame
     /// </summary>
     public class PlayerManager : MonoBehaviourPunCallbacks
     {
+        private float inputX, inputZ;
+        public float moveSpeed;
+        public Rigidbody theRB;
         
         [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
         public static GameObject LocalPlayerInstance;
@@ -36,6 +41,24 @@ namespace Com.MyCompany.MyGame
             // #Critical
             // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
             DontDestroyOnLoad(this.gameObject);
+        }
+
+        private void FixedUpdate()
+        {
+            theRB.velocity = new Vector3(inputX * moveSpeed, theRB.velocity.y, inputZ * moveSpeed);
+        }
+
+        public void Move(InputAction.CallbackContext context)
+        {
+            if (photonView.IsMine)
+            {
+                if (context.performed)
+                {
+                    inputX = context.ReadValue<Vector2>().x;
+
+                    inputZ = context.ReadValue<Vector2>().y;
+                }
+            }
         }
     }
 }
