f45a195a6310c637da8a0516dc032a87a3bd4600
ChristopherBuilderman <61890951+ChristopherBuilderman@users.noreply.github.com>
2022-04-23 19:46:58 -0600

    Adding in Character Creator files
    Adding in post-pipeline-change

---
+++ gimmStudio/Assets/CharacterCreator/Scripts/CharacterCreatorGUI.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.SceneManagement;
+using ColorPicker;
+using UnityEngine.UI;
+
+public class CharacterCreatorGUI : MonoBehaviour
+{
+
+    [SerializeField] private CharacterCreatorSO characterData;
+
+    [SerializeField] private List<Button> colorButtons;
+
+    [SerializeField] private Transform headAnchor;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private GameObject colorPickerCanvasPrefab;
+    private ColorWheelControl colorWheelControl;
+    private GameObject _activeColorPickerCanvas;
+    private bool colorOpen;
+    private int spinDirection = -3;
+    [SerializeField] private int colorIndex;
+    private bool vrUser;
+    [SerializeField] private Transform colorPickerLocation;
+
+    private void Awake()
+    {
+        if (PlayerPrefs.GetInt("vrEnabled") == 0)
+        {
+            vrUser = true;
+        }
+
+        else if (PlayerPrefs.GetInt("vrEnabled") == 1)
+        {
+            vrUser = false;
+        }
+    }
+    public void OpenColorPicker(int i)
+    {
+        Debug.Log("step 1: opencolorpicker");
+        if (colorPickerCanvasPrefab == null || _activeColorPickerCanvas != null) return;
+        Debug.Log("step 2: not null");
+        colorOpen = true;
+        if (vrUser)
+        {
+            _activeColorPickerCanvas = Instantiate(colorPickerCanvasPrefab, colorPickerLocation);
+        }
+        else
+        {
+            _activeColorPickerCanvas = Instantiate(colorPickerCanvasPrefab);
+        }
+        
+        Debug.Log("step 3: Instantiate");
+        _activeColorPickerCanvas.transform.GetChild(0).Find("CloseButton").GetComponent<Button>().onClick.AddListener(CloseColorPicker);
+        Debug.Log("step 4: addListener");
+        colorIndex = i;
+        /* colorIndex ints:
+         * 0 = hair
+         * 1 = eyes
+         * 2 = top
+         * 3 = bottom
+         * 4 = hat
+         * 5 = mask
+         * 6 = wings
+         * 7 = hands
+         */
+        colorWheelControl = _activeColorPickerCanvas.transform.GetChild(0).GetComponent<ColorWheelControl>();
+        Debug.Log("step 5: set colorWheelControl");
+        Color c = characterData.GetLastColor(i);
+        Debug.Log("step 6: Set Color: " + c);
+        if (c == Color.white) colorWheelControl.SetDefaultColor();
+        colorWheelControl.PickColor(c);
+        colorWheelControl.UpdateMaterial();
+        colorWheelControl.UpdateColor();
+    }
+    
+    private void CloseColorPicker()
+    {
+        colorOpen = false;
+        characterData.ApplyColor(colorIndex, colorWheelControl.Selection);
+        colorButtons[colorIndex].GetComponent<Image>().color = colorWheelControl.Selection;
+        Destroy(_activeColorPickerCanvas);
+    }
+
+    public void PlaySound()
+    {
+        audioSource.Play();
+    }
+
+    private void Start()
+    {
+        characterData.LoadCharacterOnCreator(headAnchor,true);
+    }
+
+    private void Update()
+    {
+        if (colorOpen)
+        {
+            characterData.ApplyColor(colorIndex, colorWheelControl.Selection);
+        }
+        
+        spinDirection = colorOpen ? 1 : -3;
+        transform.Rotate(Vector3.up, Time.deltaTime * 10f * spinDirection);
+    }
+
+    private void SaveFile()
+    {
+        string destination = Application.persistentDataPath + "/charprefs.dat";
+
+        FileStream file = File.Exists(destination) ? File.OpenWrite(destination) : File.Create(destination);
+
+        CharacterPreferences charPrefs = new CharacterPreferences(
+            characterData._hairIndex, characterData._hairColor,
+            characterData._eyesIndex, characterData._eyeColor,
+            characterData._topIndex, characterData._topColor,
+            characterData._bottomIndex, characterData._bottomColor,
+            characterData._hatIndex, characterData._hatColor,
+            characterData._maskIndex, characterData._maskColor,
+            characterData._wingsIndex, characterData._wingsColor,
+            characterData._handsIndex, characterData._handsColor
+        );
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, charPrefs);
+        file.Close();
+    }
+
+    public void ExecuteButtonHandler()
+    {
+        SaveFile();
+    }
+
+    public void BoolHair()
+    {
+        characterData._hair = !characterData._hair;
+        characterData.ApplyModifierOnCreator(CharacterCreatorSO.ModelDetail.HAIR,characterData._hair,characterData._hairIndex,headAnchor);
+    }
+    
+    public void BoolEyes()
+    {
+        characterData._hat = !characterData._eyes;
+        //TODO: Create custom method for eyes
+    }
+    
+    public void BoolTop()
+    {
+        characterData._top = !characterData._top;
+        characterData.ApplyModifierOnCreator(CharacterCreatorSO.ModelDetail.TOP,characterData._top,characterData._topIndex,headAnchor);
+    }
+    
+    public void BoolBottom()
+    {
+        characterData._bottom = !characterData._bottom;
+        characterData.ApplyModifierOnCreator(CharacterCreatorSO.ModelDetail.BOTTOM,characterData._bottom,characterData._bottomIndex,headAnchor);
+    }
+    
+    public void BoolHat()
+    {
+        characterData._hat = !characterData._hat;
+        characterData.ApplyModifierOnCreator(CharacterCreatorSO.ModelDetail.HAT,characterData._hat,characterData._hatIndex,headAnchor);
+    }
+    
+    public void BoolMask()
+    {
+        characterData._mask = !characterData._mask;
+        characterData.ApplyModifierOnCreator(CharacterCreatorSO.ModelDetail.MASK,characterData._mask,characterData._maskIndex,headAnchor);
+    }
+    
+    public void BoolWings()
+    {
+        characterData._wings = !characterData._wings;
+        characterData.ApplyModifierOnCreator(CharacterCreatorSO.ModelDetail.WINGS,characterData._wings,characterData._wingsIndex,headAnchor);
+    }
+    
+    public void BoolHands()
+    {
+        characterData._hands = !characterData._hands;
+        characterData.ApplyModifierOnCreator(CharacterCreatorSO.ModelDetail.HANDS,characterData._hands,characterData._handsIndex,headAnchor);
+    }
+}
---
+++ gimmStudio/Assets/CharacterCreator/TestPicker.shader
@@ -0,0 +1,146 @@
+﻿Shader "UI/ColorWheel" {
+	Properties {
+		_Color ("_Color", Color) = (1,1,1,1)
+		_OutlineColor ("_OutlineColor", Color) = (0,0,0,1)
+		_OutlineSize ("_OutlineSize", Range(0,1)) = 0.5
+	}
+	SubShader {
+		Lighting Off
+		Blend SrcAlpha OneMinusSrcAlpha
+	
+		Pass{		
+			CGPROGRAM
+			#pragma vertex vert
+	        #pragma fragment frag
+			#pragma target 3.0
+			
+			#include "UnityCG.cginc"
+			
+			//Prepare the inputs
+			struct vertIN{
+				float4 vertex : POSITION;
+				float4 texcoord0 : TEXCOORD0;
+			};
+			
+			struct fragIN{
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+			
+			//Function for making smooth circles from gradient
+			fixed smoothCircle(fixed size, fixed gradient){
+				fixed scaleFactor = size + 1;
+				return smoothstep(0.5 - 0.0025 * scaleFactor, 0.5 + 0.0025 * scaleFactor, 1 - gradient * scaleFactor);
+			}
+			
+			//Function for making box from gradient
+			fixed smoothBox(fixed size, fixed2 gradient){
+				fixed scaleFactor = size * 0.5;
+				fixed alpha = ceil(gradient.x - scaleFactor);
+				alpha *= ceil((1 - gradient.x) - scaleFactor);
+				alpha *= ceil(gradient.y - scaleFactor);
+				alpha *= ceil((1 - gradient.y) - scaleFactor);
+				
+				return alpha;
+			}
+
+			//Remap function
+			fixed remap(float from, float fromMin, float fromMax, float toMin, float toMax)
+		    {
+		        fixed fromAbs  =  from - fromMin;
+		        fixed fromMaxAbs = fromMax - fromMin;      
+		       
+		        fixed normal = fromAbs / fromMaxAbs;
+		 
+		        fixed toMaxAbs = toMax - toMin;
+		        fixed toAbs = toMaxAbs * normal;
+		 
+		        fixed to = toAbs + toMin;
+		       
+		        return to;
+		    }
+			
+			//Get the values from outside
+			fixed4 _Color;
+			float4 _OutlineColor;
+			fixed _OutlineSize;
+			
+			//Fill the vert struct
+			fragIN vert (vertIN v){
+				fragIN o;
+				
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = v.texcoord0;
+				
+				return o;
+			}
+			
+			//Draw the circle
+			fixed4 frag(fragIN i) : COLOR{
+				fixed4 c = 1;
+				
+				//Make the inner area of the box
+				fixed2 bGrad = 1;
+				bGrad.x = smoothstep(0.25, 0.75, i.uv.x);
+				bGrad.y = smoothstep(0.25, 0.75, i.uv.y);
+				
+				fixed4 cBox = lerp(1, _Color, bGrad.x) * bGrad.y;
+				
+				//Set up PI
+				fixed PI = 3.14159265359;
+
+				//Circular gradient
+				fixed cGrad = distance(i.uv, fixed2(0.5, 0.5));
+				
+				//Angle gradient
+				fixed ang = atan2(1 - i.uv.x - 0.5, 1 - i.uv.y - 0.5) + PI;
+
+				//Calculate hue
+				fixed4 cWheel = 1;
+				
+				cWheel.r = clamp(asin(cos(ang)) + 0.5, 0, 1);
+				cWheel.g = clamp(asin(cos(2 * PI * (1.0/3.0) - ang)) + 0.5, 0, 1);
+				cWheel.b = clamp(asin(cos(2 * PI * (2.0/3.0) - ang))+ 0.5, 0, 1);
+
+								
+				//Calculate white part
+				fixed aWhite = smoothCircle(0.025, cGrad);
+				
+				aWhite -= smoothCircle(0.36, cGrad);
+				
+				aWhite += smoothBox(0.47, i.uv.xy);
+				
+				c = lerp(_OutlineColor, 1, aWhite);
+				
+				//Add color
+
+				fixed v1 = remap(_OutlineSize,0,1,0,0.12);
+				fixed aCol = smoothCircle(v1, cGrad);
+
+				fixed v2 = remap(_OutlineSize,0,1,0.4,0.2);
+				aCol -= smoothCircle(v2, cGrad);
+				
+				c = lerp(c, cWheel, aCol);
+
+				fixed v3 = remap(_OutlineSize,0,1,0.45,0.55);
+				aCol = smoothBox(v3, i.uv.xy);
+				
+				c = lerp(c, cBox, aCol);
+				
+				//Set alpha
+				fixed alpha = smoothCircle(0, cGrad);
+				
+				alpha -= smoothCircle(0.4, cGrad);
+				
+				alpha += smoothBox(0.45, i.uv.xy);
+
+				c.a = alpha;
+				
+				return c;
+			}
+			
+			ENDCG
+			
+		}
+	} 
+}
